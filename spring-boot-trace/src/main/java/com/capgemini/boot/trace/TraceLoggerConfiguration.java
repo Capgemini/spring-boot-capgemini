676336ffa28eb280eadda0c224dd473daa18bc49
rhart <russellxhart@googlemail.com>
2015-04-13 16:22:32 +0100

    Add checkstyle to the build, fixes #5

--- spring-boot-trace/src/main/java/com/capgemini/boot/trace/TraceLoggerConfiguration.java
+++ spring-boot-trace/src/main/java/com/capgemini/boot/trace/TraceLoggerConfiguration.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2015 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.capgemini.boot.trace;
 
 import org.springframework.aop.Advisor;
@@ -8,21 +23,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-import com.capgemini.boot.trace.annotation.Trace;
-
 /**
  * Configures basic trace logging for enabled spring-boot applications.
  * 
  * Currently, only methods annotated with the @Trace annotation will
  * be traced.
- * 
- * @author Craig Williams
- *
  */
 @Configuration
-@EnableAspectJAutoProxy(proxyTargetClass=true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class TraceLoggerConfiguration {
-	@Bean(name = "traceInterceptor")
+    @Bean(name = "traceInterceptor")
     public CustomizableTraceInterceptor customizableTraceInterceptor() {
         CustomizableTraceInterceptor customizableTraceInterceptor = new CustomizableTraceInterceptor();
         customizableTraceInterceptor.setUseDynamicLogger(true);
--- spring-boot-trace/src/test/groovy/com/capgemini/boot/trace/TestApplication.java
+++ spring-boot-trace/src/test/groovy/com/capgemini/boot/trace/TestApplication.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2015 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.capgemini.boot.trace;
 
 import com.capgemini.boot.trace.annotation.EnableTraceLogger;
@@ -8,10 +23,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @EnableTraceLogger
-public class TestApplication {	
-	
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(TestApplication.class).properties(
-				"spring.application.name=traceLogger").run(args);
-	}
+public class TestApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(TestApplication.class).properties(
+            "spring.application.name=traceLogger").run(args);
+    }
 }
